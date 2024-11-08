6f7ae0dd387b5ce0355d5ad6c28c2e1d6a3be993
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-08 19:56:53 +1100

    inital functionality and ui completed

---
+++ script.js
@@ -0,0 +1,56 @@
+const input = document.querySelector(".input-task");
+
+const errorMsg = document.querySelector(".error-message");
+
+const addBtn = document.querySelector("#add-btn");
+
+const toDoList = document.querySelector("#to-do-list-container");
+
+const listContainer = document.createElement("ul");
+listContainer.classList.add("list");
+
+addBtn.addEventListener("click", (e) => {
+  errorMsg.style.display = "none";
+  const val = input.value;
+
+  if (!val) {
+    errorMsg.style.display = "block";
+    return;
+  }
+
+  let lists = createlist(val);
+
+  if (!lists) return;
+
+  toDoList.appendChild(lists);
+});
+
+function createlist(value) {
+  const li = document.createElement("li");
+
+  const listText = document.createElement("input");
+  listText.readOnly = true;
+  listText.type = "text";
+  listText.value = value;
+
+  const setDiv = document.createElement("div");
+  setDiv.classList.add("set-list");
+
+  const editButton = document.createElement("i");
+  const deleteButton = document.createElement("i");
+
+  editButton.classList.add("ri-pencil-line");
+  editButton.classList.add("edit");
+
+  deleteButton.classList.add("ri-delete-bin-5-line");
+  deleteButton.classList.add("delete");
+
+  setDiv.appendChild(editButton);
+  setDiv.appendChild(deleteButton);
+
+  li.appendChild(listText);
+  li.appendChild(setDiv);
+
+  listContainer.appendChild(li);
+  return listContainer;
+}
